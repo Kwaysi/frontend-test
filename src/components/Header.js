8e41ccb8e10063f6b61e310c7dc77e6c696de8c5
David Kilson <itzkwaysi@gmail.com>
2020-02-05 19:21:38 +0100

    Added Header component and css

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import images from "../assets";
+
+// CSS Files
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="left">
+        <img src={images["logo.png"]} alt="App Logo" />
+      </div>
+      <div className="center">
+          <img src={images["search.svg"]} alt="Search Icon" />
+          <input placeholder="Search" />
+      </div>
+      <div className="right">
+        <ul>
+          <li>Support</li>
+          <li>FAQs</li>
+          <li>
+            <img src={images["notifications.svg"]} alt="Profile" />
+            <span className="count">8</span>
+          </li>
+        </ul>
+        <div className="profile">
+          <p>Hello Jide</p>
+          <img src={images["ppic.png"]} alt="Profile" />
+        </div>
+      </div>
+    </header>
+  );
+}
